0786b7c6451fb6050ce1377efcd2f88afc41ac6b
Vincent Marchal <marchal.vin@gmail.com>
2024-07-29 08:33:12 +0000

    members

---
+++ members.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    // Members
+    const members = [
+        {
+            name: 'John',
+            age: 20,
+            skills: ['HTML', 'CSS', 'JS'],
+        },
+        {
+            name: 'Jane',
+            age: 25,
+            skills: ['HTML', 'CSS', 'JS', 'React'],
+        },
+        {
+            name: 'Jack',
+            age: 30,
+            skills: ['HTML', 'CSS', 'JS', 'React', 'Node'],
+        },
+    ];
+
+    return members;
+}
